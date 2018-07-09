b62520d7983840fc4177915a004a2c8284be7006
David Maisuradze <datomaisuradze95@gmail.com>
2018-07-09 13:36:29 +0400

    AutoRegistrationInMonitoring uncommented in Startup.cs

--- src/Lykke.Job.RepositoryTgBot/Startup.cs
+++ src/Lykke.Job.RepositoryTgBot/Startup.cs
@@ -125,7 +125,7 @@ namespace Lykke.Job.RepositoryTgBot
             }
             catch (Exception ex)
             {
-                await Log.WriteFatalErrorAsync(nameof(Startup), nameof(StartApplication), "", ex);
+                await Log.WriteFatalErrorAsync(nameof(Startup), nameof(StartApplication), " ", ex);
                 throw;
             }
         }
